6c733563566b53fcf1a63f8927c039ff6fb849e6
H. Kamran <hkamran@hkamran.com>
2023-08-11 18:01:24 -0700

    Add helper function

--- dashboard/lib/dates.ts
+++ dashboard/lib/dates.ts
@@ -1,14 +1,22 @@
 /**
+ * Create a `Date` object from a date string in the format of `YYYY-MM-DD`
+ * @param date {string} A date in the format of `YYYY-MM-DD`
+ * @returns {Date} a `Date` object
+ */
+const getDateFromYYYYMMDD = (date: string): Date =>
+    new Date(
+        parseInt(date.substring(0, 4)),
+        parseInt(date.substring(5, 7)) - 1,
+        parseInt(date.substring(8, 10)),
+    );
+
+/**
  * Convert a `YYYY-MM-DD` date to a date string
  * @param date {string} A date in the format of `YYYY-MM-DD`
  * @returns {string} A date string in the format of `Month Day, Year`
  */
 export const dateToString = (date: string): string =>
-    new Date(
-        parseInt(date.substring(0, 4)),
-        parseInt(date.substring(5, 7)) - 1,
-        parseInt(date.substring(8, 10)),
-    ).toLocaleString(undefined, {
+    getDateFromYYYYMMDD(date).toLocaleString(undefined, {
         month: "long",
         day: "numeric",
         year: "numeric",
@@ -18,9 +26,11 @@ export const dateToString = (date: string): string =>
  * Get the current date in the format of `YYYY-MM-DD`
  * @returns {string} A date in the format of `YYYY-MM-DD`
  */
-export const getISODate = (): string => {const date = new Date();
-    return new Date(
-        +date - date.getTimezoneOffset() * 60 * 1000,
-    )
+export const getISODate = (
+    inputDate: string | undefined = undefined,
+): string => {
+    const date = inputDate ? getDateFromYYYYMMDD(inputDate) : new Date();
+    return new Date(+date - date.getTimezoneOffset() * 60 * 1000)
         .toISOString()
-        .split("T")[0];}
+        .split("T")[0];
+};
